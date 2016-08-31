f8992f3a14f55cb9d93e2e86f24b5023c0c87862
4015a13 <4015a13@anon.ece.uprm.edu>
2016-08-31 08:08:35 -0400

    The size of the Window was changed and centered.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,14 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Diva L. Mockford Polanco");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+		//myFrame.setLocation(200, 200);
+		myFrame.setSize(200, 200);
+		//Found this method in the Java API that centers your screen depending on your main monitor.
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setVisible(true);
 	}
 }
